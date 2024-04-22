51984192b853d19cb415bb6c4af527ae942e03a0
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2024-04-22 12:58:02 +0700

    update port on package json

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "online-classroom-pens-streaming-v1",
   watch: true,
-  script: "bun run start -p 4000",
+  script: "bun run start",
   ignore_watch: ["node_modules", "dist", "logs", "public"],
   interpreter: "bun",
   exec_mode: "cluster",
